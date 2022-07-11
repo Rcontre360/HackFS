084022a86ed9f811100ecb75cb2573bb5d35e478
Rcontre360 <rcontreraspimentel@gmail.com>
2022-07-11 18:03:47 -0400

    storage functions finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import type {NextPage} from "next";
-import Head from "next/head";
-
-import Layout from "@components/layout";
+import {uploadDirectory} from "@helpers/storage/ipfs";
 
 const Home: NextPage = () => {
+  const handleFiles = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files: (File | Object)[] = Array.from(event.target.files || []);
+    const uploadContent = files.concat([{mynewObject: "is awesome", isAwesome: true}]);
+
+    if (files.length <= 0) return;
+
+    try {
+      await uploadDirectory(uploadContent);
+    } catch (err: any) {
+      console.log({err});
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      Some home content
+      <input type="file" onChange={handleFiles}></input>
     </div>
   );
 };
